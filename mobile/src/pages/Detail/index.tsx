a5a059cbbb473ab918fa8825c406e00d95494c8b
Luis Cetta <luiscetta@gmail.com>
2020-12-08 13:11:34 -0300

    ecoleta2.0

---
+++ mobile/src/pages/Detail/index.tsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView, Linking } from 'react-native'
+import Constants from 'expo-constants'
+import { RectButton } from 'react-native-gesture-handler'
+import * as MailComposer from 'expo-mail-composer'
+
+
+const Detail = () => {
+  const navigation = useNavigation(); 
+
+  function handleNavigateBack(){
+    navigation.goBack();
+  }
+
+  function handleComposerMail() {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos'
+    })
+  }
+
+  function handleWhatsapp() {
+   Linking.openURL(`whatsapp://send?phone=${+5521972269935}&text=Tenho interesse em saber mais sobre esse ponto de coleta de resíduos.`)
+  }
+
+  return (
+   <SafeAreaView style={{ flex: 1 }}>  
+      <View style={styles.container}>
+          <TouchableOpacity onPress={handleNavigateBack}>
+            <Icon name="arrow-left" size={20} color="#34cb79"  />
+          </TouchableOpacity>
+
+          <Image 
+            style={styles.pointImage} 
+            source={{ 
+              uri: 'https://images.unsplash.com/photo-1573481078935-b9605167e06b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60' 
+            }} 
+          />
+          <Text style={styles.pointName}>
+            Mercado Azul
+          </Text>
+
+          <Text style={styles.pointItems}>
+            Lâmpadas, Óleo de Cozinha
+          </Text>
+
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}>
+            Endereço 
+            </Text>
+
+            <Text style={styles.addressContent}>
+            Rio de Janeiro, RJ
+            </Text>
+          </View>
+      </View>
+      <View style={styles.footer}>
+       <RectButton style={styles.button} onPress={handleWhatsapp}>
+        <FontAwesome name="whatsapp" size={20} color= "#FFF" />
+        <Text style={styles.buttonText}>
+           Whatsapp
+        </Text>
+       </RectButton>
+
+       <RectButton style={styles.button} onPress={handleComposerMail}>
+        <Icon name="mail" size={20} color= "#FFF" />
+        <Text style={styles.buttonText}>
+           E-mail
+        </Text>
+       </RectButton>
+      </View>
+   </SafeAreaView>  
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+
+  pointImage: {
+    width: '100%',
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 10,
+    marginTop: 32,
+  },
+
+  pointName: {
+    color: '#322153',
+    fontSize: 28,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  pointItems: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  address: {
+    marginTop: 32,
+  },
+  
+  addressTitle: {
+    color: '#322153',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+
+  addressContent: {
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  footer: {
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: '#999',
+    paddingVertical: 20,
+    paddingHorizontal: 32,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  
+  button: {
+    width: '48%',
+    backgroundColor: '#34CB79',
+    borderRadius: 10,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    marginLeft: 8,
+    color: '#FFF',
+    fontSize: 16,
+    fontFamily: 'Roboto_500Medium',
+  },
+});
+export default Detail;
